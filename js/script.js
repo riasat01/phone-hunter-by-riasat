6133096474eb458d95671ed2aac2fa3c8b5ebd94
riasat01 <riasat01@outlook.com>
2023-08-28 12:58:22 +0600

    show all and less button and spinner added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-function getPhone(url) {
+function getPhone(url = 'a', all) {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${url}`)
         .then(res => res.json())
-        .then(tottho => displayPhones(tottho.data))
+        .then(tottho => displayPhones(tottho.data, all))
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, all) => {
     console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
+    const showBtn = document.getElementById('show-all');
+    const showLess = document.getElementById('show-less');
+    if (phones.length > 12 && all === false) {
+        showBtn.removeAttribute('disabled');
+        showLess.setAttribute('disabled', true);
+        phones = phones.slice(0, 12);
+    } else {
+        showBtn.setAttribute('disabled', true);
+        showLess.removeAttribute('disabled');
+    }
     phones.forEach(phone => {
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card p-4 bg-base-100 shadow-xl`;
@@ -24,12 +34,41 @@ const displayPhones = phones => {
         </div>
         `;
         phoneContainer.appendChild(phoneCard);
+
     });
+    spinner(false);
 }
 
 // searchPhone
 
-function searchPhone(){
-const phone = document.getElementById('search-phone').value;
-getPhone(phone);
+function searchPhone(all = false) {
+    spinner(true);
+    const phone = document.getElementById('search-phone').value;
+    getPhone(phone, all);
+}
+
+// show  all
+
+document.querySelector('#show-all').addEventListener('click', () => {
+    const callFromShowAll = true;
+    searchPhone(callFromShowAll);
+});
+
+// show less
+document.querySelector('#show-less').addEventListener('click', () => {
+    // const callFromShowAll = true;
+    searchPhone();
+})
+
+// getPhone();
+
+// loading spinner
+
+const spinner = (isLoading) => {
+    const loading = document.querySelector("#spinner");
+    if (isLoading) {
+        loading.classList.remove('hidden');
+    } else {
+        loading.classList.add('hidden');
+    }
 }
