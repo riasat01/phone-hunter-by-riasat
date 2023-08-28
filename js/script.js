a4fe09d0f133612fae59b26f239d88575879583c
riasat01 <riasat01@outlook.com>
2023-08-28 14:49:27 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function getPhone(url = 'a', all) {
 }
 
 const displayPhones = (phones, all) => {
-    console.log(phones);
+    // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
     const showBtn = document.getElementById('show-all');
@@ -28,7 +28,7 @@ const displayPhones = (phones, all) => {
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div class="card-actions justify-end">
-                        <button class="w-full btn btn-primary">Buy Now</button>
+                        <button onclick="handleDetails('${phone.slug}')" class="w-full btn btn-primary">Buy Now</button>
                     </div>
                 </div>
         </div>
@@ -37,6 +37,42 @@ const displayPhones = (phones, all) => {
 
     });
     spinner(false);
+}
+
+// show details
+
+const handleDetails = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+        .then(res => res.json())
+        .then(tottho => displayDetails(tottho.data))
+}
+
+// display details
+
+const displayDetails = (data) => {
+    console.log(data);
+    const modal = document.querySelector('#phoneDtails').childNodes[1];
+    // console.log(modal.childNodes[1].childNodes);
+    const div = document.createElement('div');
+    const details = `
+    <img src="${data.image}" alt="phone image" />
+    <p>${data.name}</p>
+    <p>Storage: ${data?.mainFeatures?.storage}</p>
+    <p>Display Size: ${data?.mainFeatures?.displaySize}</p>
+    <p>Chipset: ${data?.mainFeatures?.chipSet}</p>
+    <p>Memory: ${data?.mainFeatures?.memory}</p>
+    <p>Slug: ${data.slug}</p>
+    <p>Release Date: ${data.releaseDate}</p>
+    <p>Brand: ${data.brand}</p>
+    <p>GPS: ${data?.others?.GPS}</p>
+    <div class="modal-action">
+        <button class="btn">Close</button>
+    </div>
+    `;
+    modal.textContent = '';
+    div.innerHTML = details;
+    modal.insertBefore(div, modal.childNodes[3]);
+    phoneDtails.showModal()
 }
 
 // searchPhone
